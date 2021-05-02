d02b4c3253bb03fba0689a132b236562fd564fb8
gtook101 <78670851+gtook101@users.noreply.github.com>
2021-05-01 20:04:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependencies
 const express = require("express");
 const path = require("path");
+const route = express.Router;
 
 const fs = require("fs");
 
@@ -9,26 +10,46 @@ const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //Routes
 app.get("/", (req, res) => res.sendFile(path.join(_dirname, "index.html")));
 
-app.get("./notes", function (req, res) {
-  res.sendFile(path.join(_dirname, "notes.html"));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(_dirname, "notes.html"))
+);
+
+app.get("/notes", (req, res) => {
+  res.json(newNoteDatabase);
 });
 
+app.post("notes", (req, res) => {
+  console.log("Creating new note");
+  const newNote = req.body;
+  if (!newDataNote.title || !newDataNote.text) {
+    throw new Error("please enter information");
+  }
+
+  newDataNote.id = uuid.v1();
+  let updateDatabase = newNoteDatabase;
+  updatedDatabase.push(newDataNote);
+  fs.writeFile("db.json", JSON.stringify(updatedDatabase), (err) => {
+    if (err) throw err;
+    console.log("success!");
+  });
+});
 //Read the db file
 const handleRequest = (req, res) => {
-  fs.readFile(`${_dirname}/notes.html`);
+  fs.readFile(`${_dirname}db.json`);
 };
 
 // Create our server
-const server = http.createServer(handleRequest);
+//const server = http.createServer(handleRequest);
 
 //Add a note to db file
-const writeable = fs.writeFile("/api/notes", function (req, res) {
-  res.sendFile(path.join(_dirname, "db.json"));
-});
+//const writeable = fs.writeFile("./db.json", function (req, res) {
+//res.sendFile(path.join(_dirname, "db.json"));
+//});
 
-//Listener
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+//
+//
